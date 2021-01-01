1320dbecc2a606e1e8f19277abea3e060ed1b016
Antonio Caravello <acaravello@xeniaprogetti.it>
2021-01-01 12:11:42 +0100

    Added icons and navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import "./Navbar.css";
+
+class Navbar extends Component {
+    render() {
+        return (
+            <div className="Navbar">
+                Navbar here
+            </div>
+        )
+    }
+}
